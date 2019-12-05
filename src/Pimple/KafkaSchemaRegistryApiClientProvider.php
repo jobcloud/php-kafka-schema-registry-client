40bbb9e233d5e7e35d5cb68981fbf16db0f1fecd
Milan Radovic <milan.radovic@jobcloud.ch>
2019-12-05 14:42:25 +0100

    Review Changes

--- src/Pimple/KafkaSchemaRegistryApiClientProvider.php
+++ src/Pimple/KafkaSchemaRegistryApiClientProvider.php
@@ -18,56 +18,56 @@ class KafkaSchemaRegistryApiClientProvider implements ServiceProviderInterface
 
     public const CONTAINER_KEY = 'kafka_schema_registry';
     public const SETTING_KEY_USERNAME = 'username';
-    public const PASSWORD = 'password';
-    public const BASE_URL = 'base_url';
-    public const CLIENT = 'client';
-    public const REQUEST_FACTORY = 'request_factory';
-    public const HTTP_CLIENT = 'http_client';
-    public const API_CLIENT = 'api_client';
-    public const ERROR_HANDLER = 'error_handler';
+    public const SETTING_PASSWORD = 'password';
+    public const SETTING_BASE_URL = 'base_url';
+    public const SETTING_CLIENT = 'client';
+    public const SETTING_REQUEST_FACTORY = 'request_factory';
+    public const SETTING_HTTP_CLIENT = 'http_client';
+    public const SETTING_API_CLIENT = 'api_client';
+    public const SETTING_ERROR_HANDLER = 'error_handler';
 
 
     public function register(Container $container)
     {
         $this->checkRequiredOffsets($container);
 
-        if (false === isset($container[self::CONTAINER_KEY][self::REQUEST_FACTORY])) {
-            $container[self::CONTAINER_KEY][self::REQUEST_FACTORY] = new Psr17Factory();
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_REQUEST_FACTORY])) {
+            $container[self::CONTAINER_KEY][self::SETTING_REQUEST_FACTORY] = new Psr17Factory();
         }
 
-        if (false === isset($container[self::CONTAINER_KEY][self::CLIENT])) {
-            $container[self::CONTAINER_KEY][self::CLIENT] = new Curl(
-                $container[self::CONTAINER_KEY][self::REQUEST_FACTORY]
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_CLIENT])) {
+            $container[self::CONTAINER_KEY][self::SETTING_CLIENT] = new Curl(
+                $container[self::CONTAINER_KEY][self::SETTING_REQUEST_FACTORY]
             );
         }
 
-        if (false === isset($container[self::CONTAINER_KEY][self::ERROR_HANDLER])) {
-            $container[self::CONTAINER_KEY][self::ERROR_HANDLER] = new ErrorHandler();
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_ERROR_HANDLER])) {
+            $container[self::CONTAINER_KEY][self::SETTING_ERROR_HANDLER] = new ErrorHandler();
         }
 
-        if (false === isset($container[self::CONTAINER_KEY][self::HTTP_CLIENT])) {
-            $container[self::CONTAINER_KEY][self::HTTP_CLIENT] = static function (Container $container) {
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_HTTP_CLIENT])) {
+            $container[self::CONTAINER_KEY][self::SETTING_HTTP_CLIENT] = static function (Container $container) {
                 /** @var ClientInterface $client */
-                $client = $container[self::CONTAINER_KEY][self::CLIENT];
+                $client = $container[self::CONTAINER_KEY][self::SETTING_CLIENT];
 
                 /** @var RequestFactoryInterface $psr17factory */
-                $requestFactory = $container[self::CONTAINER_KEY][self::REQUEST_FACTORY];
+                $requestFactory = $container[self::CONTAINER_KEY][self::SETTING_REQUEST_FACTORY];
 
                 return new HttpClient(
                     $client,
                     $requestFactory,
-                    $container[self::CONTAINER_KEY][self::ERROR_HANDLER],
-                    $container[self::CONTAINER_KEY][self::BASE_URL],
+                    $container[self::CONTAINER_KEY][self::SETTING_ERROR_HANDLER],
+                    $container[self::CONTAINER_KEY][self::SETTING_BASE_URL],
                     $container[self::CONTAINER_KEY][self::SETTING_KEY_USERNAME] ?? null,
-                    $container[self::CONTAINER_KEY][self::PASSWORD] ?? null
+                    $container[self::CONTAINER_KEY][self::SETTING_PASSWORD] ?? null
                 );
             };
         }
 
-        if (false === isset($container[self::CONTAINER_KEY][self::API_CLIENT])) {
-            $container[self::CONTAINER_KEY][self::API_CLIENT] = static function (Container $container) {
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_API_CLIENT])) {
+            $container[self::CONTAINER_KEY][self::SETTING_API_CLIENT] = static function (Container $container) {
                 /** @var HttpClient $client */
-                $client = $container[self::CONTAINER_KEY][self::HTTP_CLIENT];
+                $client = $container[self::CONTAINER_KEY][self::SETTING_HTTP_CLIENT];
 
                 return new KafkaSchemaRegistryApiApiClient($client);
             };
@@ -77,9 +77,12 @@ class KafkaSchemaRegistryApiClientProvider implements ServiceProviderInterface
     private function checkRequiredOffsets(Container $container)
     {
 
-        if (false === isset($container[self::CONTAINER_KEY][self::BASE_URL])) {
+        if (false === isset($container[self::CONTAINER_KEY][self::SETTING_BASE_URL])) {
             throw new LogicException(
-                sprintf('Missing schema registry URL, please set it under "%s" container offset', self::BASE_URL)
+                sprintf(
+                    'Missing schema registry URL, please set it under "%s" container offset',
+                    self::SETTING_BASE_URL
+                )
             );
         }
     }
