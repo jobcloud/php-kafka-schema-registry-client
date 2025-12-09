43859ee9d2b2c6f8faeaa4fe9ddee6423eacb8f0
Dragan Radisic <50864256+dradisic@users.noreply.github.com>
2025-12-09 01:20:43 +0100

    feat(CDA-1558): update Dockerfile for PECL extensions, refine PHPCS ruleset, and clean up ReflectionAccessTrait docblocks

--- tests/ReflectionAccessTrait.php
+++ tests/ReflectionAccessTrait.php
@@ -8,11 +8,6 @@ use ReflectionException;
 trait ReflectionAccessTrait
 {
     /**
-     * Set private/protected property.
-     *
-     * @param object &$object Instantiated object that we will run method on.
-     * @param string $propertyName Property which will be set
-     * @param mixed $newProperty New property value
      * @throws ReflectionException
      */
     final public function setProperty(object $object, string $propertyName, mixed $newProperty): void
@@ -24,10 +19,6 @@ trait ReflectionAccessTrait
     }
 
     /**
-     * Set private/protected property.
-     *
-     * @param object &$object Instantiated object that we will run method on.
-     * @param string $propertyName Property which value will be returned
      * @throws ReflectionException
      */
     final public function getPropertyValue(object $object, string $propertyName): mixed
@@ -39,12 +30,7 @@ trait ReflectionAccessTrait
     }
 
     /**
-     * Call protected/private method of a class.
-     *
-     * @param object &$object Instantiated object that we will run method on.
-     * @param string $methodName Method name to call
-     * @param array $parameters Array of parameters to pass into method.
-     *
+     * @param array<string,mixed>
      * @throws ReflectionException
      */
     final public function invokeMethod(object $object, string $methodName, array $parameters = []): mixed
